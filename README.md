c7798a0b4faf3de5e14eed73a6bcc75f34c1ea97
Colin Pollock <pollockcolin@gmail.com>
2020-03-05 22:32:51 -0800

    Adding pelican-based site

---
+++ README.md
@@ -0,0 +1,13 @@
+colinpollock.net
+================
+
+
+## Setup ##
+1. `$ python3.7 -m venv .venv`
+2. `$ source .venv/bin/activate`
+3. `$ pip install --upgrade pip`
+4. `$ pip install -r requirements.txt`
+5. `$ cd .. && git clone git@github.com:getpelican/pelican-themes.git`
+6. `$ pelican-themes --install pelican-themes/aboutwilson`
+7. `$ make devserver`
+8. Go to http://localhost:8000/
---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = "Colin Pollock"
+SITENAME = AUTHOR
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "America/Los_Angeles"
+
+DEFAULT_LANG = "en"
+
+THEME = "aboutwilson"
+GITHUB_URL = "https://github.com/colinpollock"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (
+# ("Python.org", "http://python.org/"),
+# )
+
+# Social widget
+SOCIAL = (
+    ("github", "https://github.com/colinpollock"),
+    ("linkedin", "https://www.linkedin.com/in/pollockcolin/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
